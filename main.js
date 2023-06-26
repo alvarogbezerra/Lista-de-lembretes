2c65b9714db5765658acd456bfe88b32ba4ee48a
Álvaro Bezerra <alvarogblimapx@gmail.com>
2023-06-26 17:41:53 -0300

    tudo

--- main.js
+++ main.js
@@ -1,65 +1,78 @@
-/*1-O presente projeto será um aplicativo que permita ao usuário adicionar, visualizar e excluir tarefas.
+/*
+V 1-O presente projeto será um aplicativo que permita ao usuário adicionar, visualizar e excluir tarefas.
+V 2-Cada tarefa deve conter: nome, data de realização e estado (fazer ou feita).
+F 3-O aplicativo deve permitir a listagem de tarefas ordenadas por nome e data de realização, nessa listagem deve aparecer somente as tarefas com estado "fazer".
+F 4-O aplicativo deve permitir a listagem de tarefas "feita"
+F 5-O aplicativo deve permitir que o usuário altere e remova uma tarefa cadastrada.
 
-2-Cada tarefa deve conter: nome, data de realização e estado (fazer ou feita).
-3-O aplicativo deve permitir a listagem de tarefas ordenadas por nome e data de realização, nessa listagem deve aparecer somente as tarefas com estado "fazer".
-4-O aplicativo deve permitir a listagem de tarefas "feita"
-5-O aplicativo deve permitir que o usuário altere e remova uma tarefa cadastrada.
-
-const prompt = require('readline-sync')
-prompt.question()
-
-pedir que o chat gpt desenvolva melhor o código
+cd "C:\Users\alvar\REPOSITÓRIO PRINCIPAL\Lista de lembretes"
+node main.js
 */
 
-const prompt = require('readline-sync')
-let listaDeLembretes = [];
-let i = "node main.js"
+const prompt = require('readline-sync');
 
-function aFazerOuFeita() {
-    let aux = situacao
-    if (aux == 1) {
-        situacao = "A fazer";
-    } else if (aux == 2) {
-        situacao = "Feita";
-    } else {
-        console.log("Entrada inválida. Tente novamente.");
-        return aFazerOuFeita();
+let listaDeLembretes = [];
+
+console.log(`Esses são os seus lembretes atuais:\n ${listaDeLembretes}`);
+
+let interruptor = prompt.question("Insira 1 para iniciar o app lembretes ou 2 para encerrar");
+
+while (interruptor === "1") {
+    adicionarLembrete();
+    visualizarLembrete();
+    excluirLembrete();
+
+    interruptor = prompt.question("Insira 1 para iniciar o app lembretes ou 2 para encerrar");
+    if (interruptor === "2") {
+        break;
     }
 }
 
-
-
-
 function adicionarLembrete() {
-    let nome = "alvaro" //prompt.question("Insira o nome do lembrete: ");
-
-    //Faz com que a data fique obrigatoriamente no formato DD/MM/AAAA
-    let data = "25/07/2002" //prompt.question("Insira a data no formato DD/MM/AAAA: ");
+    let nome = prompt.question("Insira o nome do lembrete: ");
+    let data = prompt.question("Insira a data no formato DD/MM/AAAA: ");
     let regex = /^\d{2}\/\d{2}\/\d{4}$/;
     while (!regex.test(data)) {
         console.log("Data inválida. Tente novamente.");
         data = prompt.question("Insira a data no formato DD/MM/AAAA: ");
     }
+    let situacao = prompt.question("Insira a situação do lembrete como 'feito', 'a fazer' ou outra descrição.");
+    let informacoes = "Insira aqui as informações que pretende adicionar";
 
-    let situacao = prompt.question("Insira 1 para fazer ou 2 feita: ");
-    aFazerOuFeita()
-
-    let novaNota = [nome, data, situacao]
-
-    listaDeLembretes.push(novaNota)
+    let novoLembrete = [nome, data, situacao, informacoes];
+    listaDeLembretes.push(novoLembrete);
 }
-adicionarLembrete()
 
 function visualizarLembrete() {
-    console.log(listaDeLembretes);
+    for (let i = 0; i < listaDeLembretes.length; i++) {
+        console.log(`Seus lembretes possuem os seguintes títulos: ${listaDeLembretes[i][0]}`);
+    }
+
+    for (;;) {
+        let numeroDoPrimeiroIndice = prompt.question("Insira o número do lembrete que você pretende visualizar ou 'x' para sair da visualização de lembretes.");
+
+        if (numeroDoPrimeiroIndice === "x" || numeroDoPrimeiroIndice === "X") {
+            break;
+        } else if (isNaN(numeroDoPrimeiroIndice) || numeroDoPrimeiroIndice < 1 || numeroDoPrimeiroIndice > listaDeLembretes.length) {
+            console.log("Índice inválido. Por favor, insira um número válido.");
+        } else {
+            let lembreteSelecionado = listaDeLembretes[numeroDoPrimeiroIndice - 1];
+            console.log(lembreteSelecionado);
+        }
+    }
 }
-visualizarLembrete()
 
 function excluirLembrete() {
-    let indiceDoLembrete = Number(prompt.question("Insira o numero do lembrete que pretende excluir."))-1
+    for (;;) {
+        let indiceDoLembrete = prompt.question("Insira o número do lembrete que você pretende excluir ou 'x' para sair da aba de exclusão de lembretes.");
 
-    listaDeLembretes.pop(indiceDoLembrete);
+        if (indiceDoLembrete === "x" || indiceDoLembrete === "X") {
+            break;
+        } else if (isNaN(indiceDoLembrete) || indiceDoLembrete < 1 || indiceDoLembrete > listaDeLembretes.length) {
+            console.log("Índice inválido. Por favor, insira um número válido.");
+        } else {
+            let indice = parseInt(indiceDoLembrete) - 1;
+            listaDeLembretes.splice(indice, 1);
+        }
+    }
 }
-excluirLembrete()
-
-console.log(listaDeLembretes);
