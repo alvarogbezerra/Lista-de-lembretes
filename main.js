bc79bd3aa82f5ec43c74c41fbebd743ef90f3eab
Álvaro Bezerra <alvarogblimapx@gmail.com>
2023-07-03 10:32:59 -0300

    Removi caracteres que estavam com problema de codificação como: "ú", "ção" e etc

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const prompt = require('readline-sync');
+const readlineSync = require('readline-sync');
 
 //lembretes criados para teste
 let listaDeLembretes = [
@@ -22,7 +22,7 @@ function exibirMenu(){
     console.log('4 - Alterar Lembretes')
     console.log('5 - Excluir Lembrete')
     console.log('0 - Sair')
-    return Number(prompt.question('Insira o número da opção desejada:'))
+    return Number(readlineSync.question('Insira o numero da opcao desejada:'))
 }
 
 let interruptor = exibirMenu()
@@ -54,20 +54,20 @@ while (interruptor !== 0) {
 
 //Função para adicionar lembrete e para cancelar operação de adição de novos lembretes(Rodrigo)
 function adicionarLembrete() {
-      let nome = prompt.question("Insira o nome do lembrete: ");
-      let data = prompt.question("Insira a data no formato DD/MM/AAAA: ");
+      let nome = readlineSync.question("Insira o nome do lembrete: ");
+      let data = readlineSync.question("Insira a data no formato DD/MM/AAAA: ");
       let regex = /^\d{2}\/\d{2}\/\d{4}$/;
       while (!regex.test(data)) {
-          console.log("Data inválida. Tente novamente.");
-          data = prompt.question("Insira a data no formato DD/MM/AAAA: ");
+          console.log("Data invalida. Tente novamente.");
+          data = readlineSync.question("Insira a data no formato DD/MM/AAAA: ");
       }
       let situacao;
       while (true) {
-        situacao = prompt.question("Digite a situação do lembrete (1 para 'Feito', 2 para 'A fazer', ou 0 para não adicionar lembrete): ");
+        situacao = readlineSync.question("Digite a situacao do lembrete (1 para 'Feito', 2 para 'A fazer', ou 0 para nao adicionar lembrete): ");
         if (situacao === '0' || situacao === '1' || situacao === '2') {
           break;
         } else {
-          console.log("Opção inválida. Por favor, digite 0, 1 ou 2.");
+          console.log("Opcao invalida. Por favor, digite 0, 1 ou 2.");
         }
       }
       if (situacao === '1'){
@@ -75,22 +75,22 @@ function adicionarLembrete() {
       }else if (situacao === '2'){
         situacao === 'A fazer'
       }else {
-        console.log('Operação cancelada. Não foi adicionado nenhum lembrete.')
+        console.log('Operacao cancelada. Nao foi adicionado nenhum lembrete.')
         return
       }
        
-      let informacoes = prompt.question("Insira aqui as informações que pretende adicionar ao lembrete. ");
+      let informacoes = readlineSync.question("Insira aqui as informacoes que pretende adicionar ao lembrete. ");
   
       let novoLembrete = [nome, data, situacao, informacoes];
       listaDeLembretes.push(novoLembrete);
   }
   
   function ordenar() {
-    let ordenarOuNao = Number(prompt.question("Insira 1 para ordenar lembretes com o estado 'A fazer', 2 para ordenar com 'Feito' ou 3 para seguir em frente. "));
+    let ordenarOuNao = Number(readlineSync.question("Insira 1 para ordenar lembretes com o estado 'A fazer', 2 para ordenar com 'Feito' ou 3 para seguir em frente. "));
     
     while (ordenarOuNao >= 1 && ordenarOuNao <= 3) {
       if (ordenarOuNao === 1) {
-        let ordenaPorNomeOuData = Number(prompt.question("Insira 1 para ordenar lembretes 'A fazer' por nome ou 2 para ordenar por data. "));
+        let ordenaPorNomeOuData = Number(readlineSync.question("Insira 1 para ordenar lembretes 'A fazer' por nome ou 2 para ordenar por data. "));
     
         if (ordenaPorNomeOuData === 1) {
           let lembretesAFazer = listaDeLembretes.filter(function(lembrete) {
@@ -134,13 +134,13 @@ function adicionarLembrete() {
             console.log("Lembretes 'A fazer' ordenados por data:");
             console.log(lembretesAFazer);
           } else {
-            console.log("Não há lembretes com o estado 'A fazer'.");
+            console.log("Nao ha lembretes com o estado 'A fazer'.");
           }
         }
       }
     
       if (ordenarOuNao === 2) {
-        let ordenaPorNomeOuData = Number(prompt.question("Insira 1 para ordenar lembretes 'Feito' por nome ou 2 para ordenar por data. "));
+        let ordenaPorNomeOuData = Number(readlineSync.question("Insira 1 para ordenar lembretes 'Feito' por nome ou 2 para ordenar por data. "));
     
         if (ordenaPorNomeOuData === 1) {
           let lembretesFeitos = listaDeLembretes.filter(function(lembrete) {
@@ -164,7 +164,7 @@ function adicionarLembrete() {
             console.log("Lembretes 'Feito' ordenados por nome:");
             console.log(lembretesFeitos);
           } else {
-            console.log("Não há lembretes com o estado 'Feito'.");
+            console.log("Nao ha lembretes com o estado 'Feito'.");
           }
         }
     
@@ -184,28 +184,28 @@ function adicionarLembrete() {
             console.log("Lembretes 'Feito' ordenados por data:");
             console.log(lembretesFeitos);
           } else {
-            console.log("Não há lembretes com o estado 'Feito'.");
+            console.log("Nao ha lembretes com o estado 'Feito'.");
           }
         }
       }
     
       if (ordenarOuNao === 3) {
-        console.log("Você escolheu seguir em frente.");
+        console.log("Voce escolheu seguir em frente.");
         break;
       }
     
-      ordenarOuNao = Number(prompt.question("Insira 1 para ordenar lembretes com o estado 'A fazer', 2 para ordenar com 'Feito' ou 3 para seguir em frente. "));
+      ordenarOuNao = Number(readlineSync.question("Insira 1 para ordenar lembretes com o estado 'A fazer', 2 para ordenar com 'Feito' ou 3 para seguir em frente. "));
     }
     
-    console.log("Opção inválida.");
+    console.log("Opcao invalida.");
   }
   
   
   function alterar() {
-    let indiceDoLembrete = prompt.question("Insira o número do lembrete que você deseja alterar: ");
+    let indiceDoLembrete = readlineSync.question("Insira o numero do lembrete que voce deseja alterar: ");
   
     if (isNaN(indiceDoLembrete) || indiceDoLembrete < 1 || indiceDoLembrete > listaDeLembretes.length) {
-      console.log("Índice inválido. Por favor, insira um número válido.");
+      console.log("Indice invalido. Por favor, insira um numero valido.");
       return;
     }
   
@@ -215,22 +215,22 @@ function adicionarLembrete() {
     console.log("Lembrete selecionado:");
     console.log(lembreteSelecionado);
   
-    let novoNome = prompt.question("Insira o novo nome do lembrete: ");
-    let novaData = prompt.question("Insira a nova data no formato DD/MM/AAAA: ");
+    let novoNome = readlineSync.question("Insira o novo nome do lembrete: ");
+    let novaData = readlineSync.question("Insira a nova data no formato DD/MM/AAAA: ");
   
     let regex = /^\d{2}\/\d{2}\/\d{4}$/;
     while (!regex.test(novaData)) {
-      console.log("Data inválida. Tente novamente.");
-      novaData = prompt.question("Insira a nova data no formato DD/MM/AAAA: ");
+      console.log("Data invalida. Tente novamente.");
+      novaData = readlineSync.question("Insira a nova data no formato DD/MM/AAAA: ");
     }
   
     let novaSituacao;
     while (true) {
-      novaSituacao = Number(prompt.question("Para definir a nova situação do lembrete, digite 1 para 'Feito' ou 2 para 'A fazer': "));
+      novaSituacao = Number(readlineSync.question("Para definir a nova situacao do lembrete, digite 1 para 'Feito' ou 2 para 'A fazer': "));
       if (novaSituacao === 1 || novaSituacao === 2) {
         break;
       } else {
-        console.log("Opção inválida. Por favor, digite 1 ou 2.");
+        console.log("Opcao invalida. Por favor, digite 1 ou 2.");
       }
     }
   
@@ -242,7 +242,7 @@ function adicionarLembrete() {
       novaSituacao = "A fazer";
     }
   
-    let novasInformacoes = prompt.question("Insira as novas informações para o lembrete: ");
+    let novasInformacoes = readlineSync.question("Insira as novas informacoes para o lembrete: ");
   
     listaDeLembretes[indice] = [novoNome, novaData, novaSituacao, novasInformacoes];
     console.log("Lembrete alterado com sucesso!");
@@ -251,16 +251,16 @@ function adicionarLembrete() {
   //Foi feita uma alteração para que na hora de visualizar os lembretes apareça o número correspondente ao lembrete. (Matheus)
   function visualizarLembrete() {
       for (let i = 0; i < listaDeLembretes.length; i++) {
-          console.log(`[${i + 1}] Título: ${listaDeLembretes[i][0]}`);
+          console.log(`[${i + 1}] Titulo: ${listaDeLembretes[i][0]}`);
       }
   
       for (;;) {
-          let numeroDoPrimeiroIndice = prompt.question("Insira o número do lembrete que você pretende visualizar ou 'x' para sair da visualização de lembretes.");
+          let numeroDoPrimeiroIndice = readlineSync.question("Insira o numero do lembrete que você pretende visualizar ou 'x' para sair da visualizacao de lembretes.");
   
           if (numeroDoPrimeiroIndice === "x" || numeroDoPrimeiroIndice === "X") {
               break;
           } else if (isNaN(numeroDoPrimeiroIndice) || numeroDoPrimeiroIndice < 1 || numeroDoPrimeiroIndice > listaDeLembretes.length) {
-              console.log("Índice inválido. Por favor, insira um número válido.");
+              console.log("Indice invAlido. Por favor, insira um numero valido.");
           } else {
               let lembreteSelecionado = listaDeLembretes[numeroDoPrimeiroIndice - 1];
               console.log(lembreteSelecionado);
@@ -270,16 +270,16 @@ function adicionarLembrete() {
 //Adicionada uma ordem de parada no momento em que um lembrete é excluído(Rodrigo)
   function excluirLembrete() {
       for (;;) {
-          let indiceDoLembrete = prompt.question("Insira o número do lembrete que você pretende excluir ou 'x' para sair da aba de exclusão de lembretes.");
+          let indiceDoLembrete = readlineSync.question("Insira o numero do lembrete que voce pretende excluir ou 'x' para sair da aba de exclusao de lembretes.");
   
           if (indiceDoLembrete === "x" || indiceDoLembrete === "X") {
               break;
           } else if (isNaN(indiceDoLembrete) || indiceDoLembrete < 1 || indiceDoLembrete > listaDeLembretes.length) {
-              console.log("Índice inválido. Por favor, insira um número válido.");
+              console.log("Indice invalido. Por favor, insira um numero valido.");
           } else {
               let indice = parseInt(indiceDoLembrete) - 1;
               listaDeLembretes.splice(indice, 1);
-              console.log('Lembrete excluído com sucesso!')
+              console.log('Lembrete excluido com sucesso!')
               break;
           }
       }
